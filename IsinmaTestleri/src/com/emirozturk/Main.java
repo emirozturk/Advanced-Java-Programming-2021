741c3304f666a962874b43e003ba2f09bca88de2
Emir Öztürk <emirozturk3@gmail.com>
2021-10-04 13:49:26 +0300

    Temel türler

--- IsinmaTestleri/src/com/emirozturk/Main.java
+++ IsinmaTestleri/src/com/emirozturk/Main.java
@@ -1,8 +1,13 @@
 package com.emirozturk;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        //byte 0-255
+        //short 0-65535
+        //int 0-2147483647
+        //long 0-9223372036854775807
+        System.out.println(Long.MAX_VALUE);
     }
 }
