b1797fb2a345617d7f85087e6dd593fb228fed5e
Emir Öztürk <emirozturk3@gmail.com>
2021-11-22 11:46:14 +0300

    Boş Uygulama

---
+++ Bildirim/src/main/java/com/emirozturk/Bildirim/BildirimApplication.java
@@ -0,0 +1,11 @@
+package com.emirozturk.Bildirim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildirimApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(BildirimApplication.class, args);
+	}
+}
---
+++ Bildirim/src/test/java/com/emirozturk/Bildirim/BildirimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirozturk.Bildirim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BildirimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
